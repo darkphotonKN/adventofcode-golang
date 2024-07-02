f6164814ac95e0da3ac02165afc047166e50d599
Kranti <darkphoton20@gmail.com>
2024-07-03 04:44:11 +0800

    Refactoring use go function composition with pointer recievers

--- adventofcode/dayOne2023trebuchet.go
+++ adventofcode/dayOne2023trebuchet.go
@@ -32,27 +32,54 @@ func Trebuchet() {
 	// convert byte type to string
 	text := string(data)
 
+	// incoming data
+	var treb trebuchet
+	treb.Data = text
 	// fmt.Println(text)
 
 	// convert string into an array of strings based on return
-	textSlice := parseLine(text)
+	treb.ParsedLine = treb.parseLine(treb.Data)
 
 	// filter each string to digits only
-	digitsOnlySlice := filterDigits(textSlice)
+	treb.FilteredDigits = treb.filterDigits(treb.ParsedLine)
 
 	// loop and sum up the first and last values
-	result := sumFirstAndLast(digitsOnlySlice)
+	treb.Sum = treb.sumFirstAndLast(treb.FilteredDigits)
 
-	fmt.Println(result)
+	fmt.Println("Updated sum:", treb.Sum)
 
 	// sum up the first and last digits
+
+	// 	var shortTestData = `two1nine
+	// eightwothree
+	// abcone2threexyz
+	// xtwone3four
+	// 4nineeightseven2
+	// zoneight234
+	// 7pqrstsixteen`
+	//
+	// 	parsedShortTestData := parseLine(shortTestData)
+	// 	filteredShortTestData := filterDigits(parsedShortTestData)
+
+	// fmt.Println(filteredShortTestData)
+
+	fmt.Println("Trebuchet original parsed filtered digits index 3:", treb.FilteredDigits[3])
 }
 
-func parseLine(t string) []string {
-	return strings.Split(t, "\n")
+type trebuchet struct {
+	Data           string
+	ParsedLine     []string
+	FilteredDigits []string
+	Sum            int32
 }
 
-func filterDigits(s []string) []string {
+// convert a multi-line string into a string slice per new line
+func (treb *trebuchet) parseLine(s string) []string {
+	return strings.Split(s, "\n")
+}
+
+// filter string into only digits remaining inside string
+func (treb *trebuchet) filterDigits(s []string) []string {
 	// using make to preallocate memory for the correct sized string
 	digitsOnly := make([]string, len(s))
 
@@ -72,7 +99,21 @@ func filterDigits(s []string) []string {
 	return digitsOnly
 }
 
-func sumFirstAndLast(s []string) int32 {
+// convert each string digit to numbers
+func (treb *trebuchet) convertDigitWordToNumber(s []string) []string {
+	var numberSlice []string
+
+	for i, line := range s {
+
+		// swap the word digits to numbers
+		numberSlice[i] = line
+	}
+
+	return s
+}
+
+// sums first and last digits of slice
+func (treb *trebuchet) sumFirstAndLast(s []string) int32 {
 	var sum int32 = 0
 
 	for _, text := range s {
@@ -83,16 +124,13 @@ func sumFirstAndLast(s []string) int32 {
 		// convert to the number type
 		combinedDigit := string(text[0]) + string(text[len(text)-1])
 
-		fmt.Println("combinedDigit:", combinedDigit)
 		number, err := strconv.ParseInt(combinedDigit, 10, 32)
-		fmt.Println("number:", number)
 
 		if err != nil {
 			continue
 		}
 
 		sum += int32(number)
-		fmt.Println("sum so far:", sum)
 	}
 
 	return sum
