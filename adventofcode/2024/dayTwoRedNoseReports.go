499b19a5fee382c0da1282a584b5d33a9941cfaf
Kranti <kranti@Anitas-MacBook-Pro.local>
2024-12-24 21:28:01 +0800

    day two part one done

--- adventofcode/2024/dayTwoRedNoseReports.go
+++ adventofcode/2024/dayTwoRedNoseReports.go
@@ -3,7 +3,9 @@ package adventofcode2024
 import (
 	"fmt"
 	"io"
+	"math"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -16,6 +18,7 @@ The unusual data (your puzzle input) consists of many reports, one report per li
 1 3 2 4 5
 8 6 4 4 1
 1 3 6 7 9
+
 This example data contains six reports each containing five levels.
 
 The engineers are trying to figure out which reports are safe. The Red-Nosed reactor safety systems can only tolerate levels that are either gradually increasing or gradually decreasing. So, a report only counts as safe if both of the following are true:
@@ -33,32 +36,116 @@ In the example above, the reports can be found safe or unsafe by checking those 
 So, in this example, 2 reports are safe.
 
 Analyze the unusual data from the engineers. How many reports are safe?
-
-
 */
 
 func DayTwo() {
 	fmt.Println("Day Two - Red Nose Reports  - Part 1")
-
 	file, _ := os.Open("./adventofcode/2024/data/dayTwo.txt")
 	defer file.Close()
 	data, _ := io.ReadAll(file)
 
 	report := strings.Split(strings.TrimSpace(string(data)), "\n")
 
-	// safeLevels := 0
+	fmt.Println("report:", report)
+
+	safeLevels := 0
 
 	for _, levels := range report {
 		levelsSlice := strings.Split(levels, " ")
 		safety := checkSafety(levelsSlice)
+
+		if safety {
+
+			fmt.Println("Level was safe!")
+			safeLevels++
+		}
 	}
+
+	fmt.Printf("Safe Levels: %d\n", safeLevels)
 }
 
 func checkSafety(levels []string) bool {
+	// convert string values to ints
+	levelsNum := make([]int, len(levels))
 
-	for _, level := range levels {
+	for index, str := range levels {
+		number, err := strconv.Atoi(str)
 
+		if err != nil {
+			fmt.Printf("Error when converting string to int, string number was %s, error was: %s", str, err)
+		}
+
+		levelsNum[index] = number
 	}
 
+	// default to true
+	allIncreasing := true
+	allDecreasing := true
+
+	var prevLevel int
+
+	for index, level := range levelsNum {
+		fmt.Printf("Level: %d\n", level)
+
+		// skip the first level
+		if index == 0 {
+			prevLevel = level
+			continue
+		}
+
+		// --- Check Each Rule ---
+		// compare with previous and check if any of the defaults expectations are broken ---
+
+		// -- Increasing Rule --
+		if prevLevel < level {
+			fmt.Printf("\nLevels Still INCREASING\n\n")
+			// it must mean all decreasing is now not true, set it as so
+			allDecreasing = false
+		}
+
+		// -- Decreasing Rule --
+		if prevLevel > level {
+			fmt.Printf("\nLevels Still DECREASING\n\n")
+
+			// it must mean all increasing is now not true, set it as so
+			allIncreasing = false
+		}
+
+		if allIncreasing {
+			// check if increment is too large
+			difference := math.Abs(float64(prevLevel) - float64(level))
+
+			fmt.Println("Difference was:", difference)
+
+			if difference == 0 || difference > 3 {
+				fmt.Println("This level is unsafe")
+				// unsafe
+				return false
+			}
+		}
+
+		if allDecreasing {
+			// check if decrement is too large
+			difference := math.Abs(float64(prevLevel) - float64(level))
+
+			fmt.Println("Difference was:", difference)
+
+			if difference == 0 || difference > 3 {
+				fmt.Println("This level is unsafe")
+				// unsafe
+				return false
+			}
+		}
+
+		// check if any of the rules are still holding
+		if !allDecreasing && !allIncreasing {
+			fmt.Println("Both all increasing and decreasing have failed, level unsafe!")
+			return false
+		}
+
+		prevLevel = level
+	}
+
+	// passed all checks, return true
 	return true
 }
